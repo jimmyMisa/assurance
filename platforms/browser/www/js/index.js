5113e8df56aa15ad6c35de518cef90b378dc3be6
jimmy <jimmy@misa>
2017-11-24 04:34:59 +0300

    simple design

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -18,25 +18,20 @@
  */
 var app = {
     // Application Constructor
-    initialize: function () {
-        this.bindEvents();
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function () {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
+
     // deviceready Event Handler
     //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function () {
-        app.receivedEvent('deviceready');
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
     },
+
     // Update DOM on a Received Event
-    receivedEvent: function (id) {
+    receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -47,3 +42,5 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+app.initialize();
